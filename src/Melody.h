c2309209e16193eeb6ea3fdb6a7ef5c009d7f882
Domagoj <domagoj.zoraja@fer.hr>
2020-11-02 15:13:07 +0100

    Created objects for note structures to form a melody

--- src/Melody.h
+++ src/Melody.h
@@ -6,60 +6,57 @@
 #define NIBBLE_NEW_MELODY_H
 
 #include "Pitches.h"
+#include "Audio.h"
 
-struct StartUpMelody {
+StartUpTone startUpMelody[] = {
 
-	int note[8] = {
+		{NOTE_C4, 250},
+		{NOTE_G3, 125},
+		{NOTE_G3, 125},
+		{NOTE_A3, 250},
+		{NOTE_G3, 250},
+		{0,  250},
+		{NOTE_B3, 250},
+		{NOTE_C4, 250}
 
-			NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-
-	};
-
-	int duration[8] = {
-
-			4, 8, 8, 4, 4, 4, 4, 4
-
-	};
 };
 
-struct InGameMelody {
+InGameTone inGameMelody[] = {
 
-	int note[40] = {
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
+		{NOTE_A4, 250},
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
+		{NOTE_A4, 250},
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
+		{NOTE_A4, 250},
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
+		{NOTE_A4, 250},
+		{NOTE_E5, 250},
+		{NOTE_C5, 250},
 
-			NOTE_E5, NOTE_C5, NOTE_A4,
-			NOTE_E5, NOTE_C5, NOTE_A4,
-			NOTE_E5, NOTE_C5,
-			NOTE_E5, NOTE_C5, NOTE_A4,
-			NOTE_E5, NOTE_C5, NOTE_A4,
-			NOTE_E5, NOTE_C5,
 
-			NOTE_D5, NOTE_B4, NOTE_F4,
-			NOTE_D5, NOTE_B4, NOTE_F4,
-			NOTE_D5, NOTE_B4,
-			NOTE_D5, NOTE_B4, NOTE_F4,
-			NOTE_D5, NOTE_B4, NOTE_F4,
-			NOTE_D5, NOTE_B4,
-
-	};
-
-	int duration[40] = {
-
-			250, 250, 250,
-			250, 250, 250,
-			250, 250,
-			250, 250, 250,
-			250, 250, 250,
-			250, 250,
-
-			250, 250, 250,
-			250, 250, 250,
-			250, 250,
-			250, 250, 250,
-			250, 250, 250,
-			250, 250
-
-	};
+		{NOTE_D5, 250},
+		{NOTE_B4, 250},
+		{NOTE_F4, 250},
+		{NOTE_D5, 250},
+		{NOTE_B4, 250},
+		{NOTE_F4, 250},
+		{NOTE_D5, 250},
+		{NOTE_B4, 250},
+		{NOTE_D5, 250},
+		{NOTE_B4, 250},
+		{NOTE_F4, 250},
+		{NOTE_D5, 250},
+		{NOTE_B4, 250},
+		{NOTE_F4, 250},
+		{NOTE_D5, 250},
+		{NOTE_B4, 250}
 };
-
 
 #endif //NIBBLE_NEW_MELODY_H
