41ed86c98c44329f71339f5980f0ddb73776b363
Domagoj <domagoj.zoraja@fer.hr>
2020-11-02 15:12:17 +0100

    Created audio.h file for note structures

---
+++ src/Audio.h
@@ -0,0 +1,22 @@
+//
+// Created by Domagoj on 02/11/2020.
+//
+
+#ifndef NIBBLE_NEW_AUDIO_H
+#define NIBBLE_NEW_AUDIO_H
+
+struct StartUpTone {
+
+	int note;
+	int duration;
+
+};
+
+struct InGameTone {
+
+	int note;
+	int duration;
+
+};
+
+#endif //NIBBLE_NEW_AUDIO_H
