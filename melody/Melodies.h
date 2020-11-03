27d7fb8662e02cf229d3e4aae288fa37619342dd
Domagoj <domagoj.zoraja@fer.hr>
2020-11-03 14:56:10 +0100

    added enum variable

---
+++ melody/Melodies.h
@@ -0,0 +1,18 @@
+//
+// Created by Domagoj on 03/11/2020.
+//
+
+#ifndef NIBBLE_NEW_MELODIES_H
+#define NIBBLE_NEW_MELODIES_H
+
+enum {
+
+	START,
+	VICTORY,
+	LOSE,
+	LOOP
+
+};
+
+
+#endif //NIBBLE_NEW_MELODIES_H
