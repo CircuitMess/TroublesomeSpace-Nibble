469086f51e072bb18253d5284ab22650ce7e2c19
Domagoj <domagoj.zoraja@fer.hr>
2020-11-06 14:02:13 +0100

    changed victory melody

--- src/melody/VictoryMelody.h
+++ src/melody/VictoryMelody.h
@@ -28,53 +28,6 @@ Note victoryMelody[] = {
 		{NOTE_F3,  125},
 		{0,500},
 
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_G3,  250},
-		{NOTE_E3,  250},
-		{NOTE_C3,  250},
-		{0, 500},
-
-
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_A3,  125},
-		{NOTE_A3,  125},
-		{0, 125},
-		{NOTE_B3,  125},
-		{NOTE_B3,  125},
-		{0, 250},
-
-		{NOTE_G3,  125},
-		{NOTE_G3,  125},
-		{0, 125},
-		{NOTE_A3,  125},
-		{NOTE_A3,  125},
-		{0, 125},
-		{NOTE_B3,  125},
-		{NOTE_B3,  125},
-		{0, 250},
-
-
 };
 
 #endif //NIBBLE_NEW_VICTORYMELODY_H
