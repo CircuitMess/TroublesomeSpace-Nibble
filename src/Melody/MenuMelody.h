a9469070af209ceef9ecabfffc34883a5e1e0940
Domagoj <domagoj.zoraja@fer.hr>
2020-12-20 21:23:02 +0100

    added menu melody

--- src/Melody/MenuMelody.h
+++ src/Melody/MenuMelody.h
@@ -11,10 +11,16 @@
 
 Note menuMelody[] = {
 
-		{NOTE_D4, 187}, {NOTE_G4, 62}, {NOTE_C5, 375},
-		{NOTE_B4, 125}, {NOTE_G4, 94}, {NOTE_E4, 94}, {NOTE_A4, 94},
-		{NOTE_D5, 500}
+		{NOTE_E4, 400}, {NOTE_C4, 400},
+		{NOTE_E4, 400}, {NOTE_C4, 400},
+		{NOTE_E4, 400}, {NOTE_C4, 400},
+		{NOTE_E4, 400}, {NOTE_C4, 400},
+		{NOTE_F4, 400}, {NOTE_D4, 400},
+		{NOTE_F4, 400}, {NOTE_D4, 400},
+		{NOTE_F4, 400}, {NOTE_D4, 400},
+		{NOTE_F4, 400}, {NOTE_D4, 400},
+		
 
 };
 
-#endif //NIBBLE_NEW_MENUMELODY_H
+#endif //NIBBLE_NEW_MENUMELODY
