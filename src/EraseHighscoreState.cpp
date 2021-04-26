3aa65335072fb979d80a22f821024a483cf6e337
Domagoj <domagoj.zoraja@fer.hr>
2021-04-26 15:43:32 +0200

    add screen to EraseHighscoreState

--- src/EraseHighscoreState.cpp
+++ src/EraseHighscoreState.cpp
@@ -2,6 +2,7 @@
 #include "EraseHighscoreState.h"
 #include "ShowHighscoreState.h"
 #include "Highscore/Highscore.h"
+#include "bitmaps/states/eraseHighscoreStateScreen.hpp"
 
 EraseHighscoreState *EraseHighscoreState::instance = nullptr;
 
@@ -58,8 +59,10 @@ void EraseHighscoreState::loop(uint){
 
 void EraseHighscoreState::draw(){
 
-	baseSprite->setTextSize(1);
-	baseSprite->setTextFont(2);
+	baseSprite->drawIcon(eraseHighscoreStateScreen, 0, 0, 128, 128);
+
+	baseSprite->setTextSize(2);
+	baseSprite->setTextFont(1);
 	baseSprite->setTextColor(TFT_GOLD);
 
 	baseSprite->setCursor(20,20);
